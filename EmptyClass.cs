d6ad6862a654bb8c4ee2ad0e90d47ccb88b3825d
petrikpopov <112963523+petrikpopov@users.noreply.github.com>
2023-02-15 21:05:28 +0200

    Add files via upload

---
+++ EmptyClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+
+namespace _15._02._2023
+{
+    public class Book: ICloneable, IComparable
+    {
+        public string Name_Book { get; set; }
+        public int Count_P { get; set; }
+
+        public Book() { }
+        public Book(string NB, int CP)
+        {
+            Name_Book = NB;
+            Count_P = CP;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name_Book:{Name_Book}\tCount_P:{Count_P}\n");
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj is Book)
+            {
+                return Count_P.CompareTo((obj as Book).Count_P);
+            }
+            throw new NotImplementedException();
+        }
+
+        public class SortByCountPage : IComparer
+        {
+            int IComparer.Compare(object x, object y)
+            {
+                if (x is Book && y is Book)
+                {
+                    return (x as Book).Count_P.CompareTo((y as Book).Count_P);
+                }
+                throw new NotImplementedException();
+            }
+        }
+        public class SortByNameBook : IComparer
+        {
+            int IComparer.Compare(object x, object y)
+            {
+                if (x is Book && y is Book)
+                {
+                    return (x as Book).Name_Book.CompareTo((y as Book).Name_Book);
+                }
+                throw new NotImplementedException();
+            }
+        }
+
+        public object Clone()
+        {
+            return new Book(Name_Book, Count_P);
+        }
+    }
+
+    public class Library : IEnumerable
+
+    {
+        Book[] mass = new Book[5];
+
+        public Library(Book[] obj)
+        {
+            mass = new Book[obj.Length];
+            for (int i = 0; i<obj.Length; i++)
+            {
+                mass[i] = new Book(obj[i].Name_Book,obj[i].Count_P);
+            }
+        }
+        public void Print()
+        {
+            for (int i =0; i<mass.Length; i++)
+            {
+                mass[i].Print();
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i<mass.Length; i++)
+            {
+                yield return mass[i];
+            }
+            throw new NotImplementedException();
+        }
+    }
+}  
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _15._02._2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Book[] obj = new Book[3];
+            obj[0] = new Book("Крассная шапочка", 12);
+            obj[1] = new Book("Зайчик", 3);
+            obj[2] = new Book("Золушка", 6);
+            foreach (Book n in obj)
+            {
+                n.Print();
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Массив упорядоченный по количеству страниц в книге");
+            Console.WriteLine("\n");
+            Array.Sort(obj, new Book.SortByCountPage());
+            foreach (Book n in obj)
+            {
+                n.Print();
+            }
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Массив упорядоченный по названию книиг");
+            Console.WriteLine("\n");
+            Array.Sort(obj, new Book.SortByNameBook());
+            foreach (Book m in obj)
+            {
+                m.Print();
+            }
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Копирование");
+            Console.WriteLine("\n");
+            Book obj1 = new Book();
+            obj1.Name_Book = "Иванушка";
+            obj1.Count_P = 4;
+            obj1.Print();
+            Book objCopy = obj1.Clone() as Book;
+            objCopy.Name_Book = "Гуси лебеди";
+            objCopy.Count_P = 13;
+            objCopy.Print();
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Book[] books = new Book[3];
+            books[0] = new Book("Варона", 5);
+            books[1] = new Book("Трансформеры",187);
+            foreach(Book a in books)
+            {
+                a.Print();
+                Library l = new Library(books);
+
+            }
+        }
+    }
+}
